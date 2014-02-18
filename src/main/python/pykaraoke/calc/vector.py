5f948c5d94e89d7b87fae58955104337d2544d8b
StuxSoftware <netzmit@gmail.com>
2014-02-19 00:18:28 +0100

    Added functions to vector class.

--- src/main/python/pykaraoke/calc/vector.py
+++ src/main/python/pykaraoke/calc/vector.py
@@ -18,7 +18,7 @@ class Vector(tuple):
         """
         Use tuples __new__ method to make sure the values are immutable.
         """
-        tuple.__new__(cls, (x, y, z))
+        return tuple.__new__(cls, (float(x), float(y), float(z)))
 
     # The components of the vector.
     x, y, z = [property(operator.itemgetter(i)) for i in range(3)]
@@ -28,6 +28,12 @@ class Vector(tuple):
         Sets the length of the vector.
         """
         return self/abs(self)*length
+
+    def normalize(self):
+        """
+        Normalizes the vectors.
+        """
+        return self/abs(self)
 
     def distance(self, other):
         """
@@ -39,7 +45,7 @@ class Vector(tuple):
         """
         Returns the length of the vector
         """
-        return math.sqrt(self.x**2 + self.y**2 + self.z**2)
+        return math.sqrt(self.length_squared)
 
     def __add__(self, other):
         """
@@ -58,6 +64,13 @@ class Vector(tuple):
         Negates a vector.
         """
         return Vector(-self.x, -self.y, -self.z)
+    __invert__ = __neg__
+
+    def __pos__(self):
+        """
+        Makes a vector positive
+        """
+        return Vector(+self.x, +self.y, +self.z)
 
     def __mul__(self, other):
         """
@@ -69,7 +82,29 @@ class Vector(tuple):
         """
         Divides a vector with another vector.
         """
-        return Vector(self.x/other, self.y/other, self.z/other)
+        return Vector(self.x/float(other), self.y/float(other), self.z/float(other))
 
+    # Comparison
+    def __nonzero__(self):
+        """
+        Checks if this is not a null vector.
+        """
+        return self.x != 0 and self.y != 0 and self.z != 0
+
+    def __eq__(self, other):
+        """
+        Checks if the two vectors are equal.
+        """
+        for index, component in enumerate(self):
+            if other[index] != component:
+                return False
+        return True
+    __ne__ = lambda self, other: not __eq__(self, other)
+
+    @property
+    def length_squared(self):
+        return self.x**2 + self.y**2 + self.z**2
+
+    # Representation
     def __repr__(self):
         return "<Vector x:%f y:%f z:%f>"%self
